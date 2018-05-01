0722dc84d3a538b22057719cafa99ec68b531333
eleverme <36473656+eleverme@users.noreply.github.com>
2018-05-01 16:23:00 +0200

    Add files via upload

---
+++ emplois.php
@@ -0,0 +1,34 @@
+<html>
+
+ <head>
+ <META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=utf-8" />
+ <title> ECE Plouf Emplois </title>
+ <link rel="stylesheet" href="emplois.css"  /> 
+ </head>
+
+
+<body>
+<center>
+	
+<h1 class="logo"><img src="logoECE.png" alt="Logo" height="81" width="289"/></h1>
+
+<div id="menu">
+  <ul id="onglets">
+    <li class="active"><a href="emplois.php"> Emplois </a></li>
+    <li><a href="accueil.html"> Accueil </a></li>
+    <li><a href="vous.html"> Vous </a></li>
+    <li><a href="reseau.html"> Votre réseau </a></li>
+  </ul>
+</div>
+
+
+
+
+
+    
+	
+	</center>
+	
+</body>	
+
+</html>
