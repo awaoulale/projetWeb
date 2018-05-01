170bfea11b623b1fea782f1df111789ddc965ca8
eleverme <36473656+eleverme@users.noreply.github.com>
2018-05-01 13:12:49 +0200

    Add files via upload

---
+++ inscription.php
@@ -0,0 +1,39 @@
+<html>
+
+ <head>
+ <META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=utf-8" />
+ <title> ECE Plouf Inscription </title>
+ <link rel="stylesheet" href="mepconnexion.css"  /> 
+ </head>
+
+
+
+
+<body>
+	
+	<form action="insciptiontraitement.php" method="post">
+	
+		<label> Nom </label>
+		<input type="text" name="nom"/><br/>
+		
+		<label> Prenom</label>
+		<input type="text" name="prenom"/><br/>
+		
+
+		<label> Mail ECE </label>
+		<input type="text" name="mail"/><br/>
+		
+		<label> Pseudo (Premiere lettre de votre prénom suivi de votre nom exemple : JSegado)</label>
+		<input type="text" name="pseudo"/><br/>
+		
+	
+		
+		<input type="submit" value="Inscription"/>
+	</form>
+	
+	
+	 
+
+</body>
+
+</html>
