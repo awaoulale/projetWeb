af8cf18b4d164613c146f734178ebce1cef1298f
awaoulale <36473458+awaoulale@users.noreply.github.com>
2018-05-03 00:58:47 +0200

    Add files via upload
    barre de menu + pied de page

---
+++ fixednavbar.php
@@ -0,0 +1,43 @@
+<html>
+
+<head>
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+		<meta charset="utf-8">
+		<title> ECEPlouf</title>
+		<meta name="generator" content="Bootply" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<!--[if lt IE 9]>
+			<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		<link href="css/styles.css" rel="stylesheet">
+</head>
+	
+<!-- Fixed navbar -->
+<div class="navbar navbar-custom navbar-inverse navbar-static-top" id="nav">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+      </div>
+      <div class="collapse navbar-collapse">
+        <ul class="nav navbar-nav nav-justified">
+          <li><a href="http://localhost:8888/OK/projetWeb/pages/accueil.php">Accueil</a></li>
+          <li><a href="http://localhost:8888/OK/projetWeb/pages/monreseau.php">Mon reseau</a></li>
+          <li><a href="http://localhost:8888/OK/projetWeb/pages/vous.php">Vous</a></li>
+          <li><a href="http://localhost:8888/OK/projetWeb/pages/notifications.php">Notifications</a></li>
+          <li><a href="http://localhost:8888/OK/projetWeb/pages/emplois.php">Emplois</a></li>
+          <li><a href="http://localhost:8888/OK/projetWeb/pages/messagerie.php">Messagerie</a></li>
+        </ul>
+      </div><!--/.nav-collapse -->
+    </div><!--/.container -->
+</div><!--/.navbar -->
+
+
+</html>
