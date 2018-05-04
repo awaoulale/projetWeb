16685eb32436fb36c557ed70e4c6a6c5be895498
awaoulale <36473458+awaoulale@users.noreply.github.com>
2018-05-04 17:36:18 +0200

    Add files via upload
    aimer OK
    ne plus aimer OK

--- newsfeed.php
+++ newsfeed.php
@@ -19,7 +19,7 @@ if(!$connect )
 else
 {
 		
-	$resultat1=mysqli_query($connect, "SELECT id_ami1, chemin, nom, prenom, publication, aime, partage, commentaire, reactions.id_doc
+	$resultat1=mysqli_query($connect, "SELECT id_ami1, chemin, nom, prenom, publication, aime, partage, commentaire, reactions.id_doc, utilisateurs.id
 										FROM amis 
 										JOIN documents ON amis.id_ami1=documents.id_auteur 
 										JOIN utilisateurs ON utilisateurs.id=amis.id_ami1 
@@ -29,53 +29,145 @@ else
 
 	if($resultat1)
 	{
+		$tableau=array("");
+		$tour=0;		
+		
+		
+		$sql=mysqli_query($connect,"SELECT id FROM utilisateurs WHERE pseudo='$pseudo'");
+		if($sql)
+		{
+			$ressql=mysqli_fetch_array($sql);
+			$id_utilisateur=$ressql["id"];
+		}
+		
+		
 		while($idami=mysqli_fetch_array($resultat1))
 		{
-			echo $idami["prenom"]. " " .$idami["nom"]. "<br/>" .$idami["publication"]. "<br/>";
+			$tour=$tour+1;
 			
-			$id_doc=$idami["id_doc"];
+			$tableau2=array($idami["chemin"]);
 			
-			//on compte le nombre de jaimes
-			$resultat2=mysqli_query($connect, "SELECT COUNT(aime) AS nb_1 FROM reactions WHERE id_doc='$id_doc' AND aime='oui'");
-			if($resultat2)
+			//condition pour ne pas afficher 2 fois la meme publication dans le fil d'actualite !
+			if (!(in_array($idami["chemin"], $tableau)))
 			{
-				while($nbaime=mysqli_fetch_array($resultat2))
-				{
-					$nb_1=$nbaime["nb_1"];
-					echo $nb_1. " j aime(s) <br/>" ;
-				}
-			}
-			
-			//on compte le nombre de partages
-			$resultat3=mysqli_query($connect, "SELECT COUNT(partage) AS nb_2 FROM reactions WHERE id_doc='$id_doc' AND partage='oui'");
-			if($resultat3)
-			{
-				while($nbpartage=mysqli_fetch_array($resultat3))
-				{
-					$nb_2=$nbpartage["nb_2"];
-					echo $nb_2. " partages(s) <br/>" ;
-				}
-			}
-			
-			//on compte le nombre de commentaires
-			$resultat4=mysqli_query($connect, "SELECT COUNT(commentaire) AS nb_3 FROM reactions WHERE id_doc='$id_doc' AND commentaire IS NOT NULL");
-			if($resultat4)
-			{
-				while($nbcommentaire=mysqli_fetch_array($resultat4))
-				{
-					$nb_3=$nbcommentaire["nb_3"];
-					echo $nb_3. " commentaire(s) <br/>" ;
-				}
-			}
-			
-			
-			echo '<img src="'.$idami["chemin"].'.jpg" alt="photo" />';
-			echo "<br/><br/><br/>";
-
-		}
-	}
-	
-	
-}	
+				$tableau=array_merge($tableau, $tableau2);
 		
+				echo "<strong>" .$idami["prenom"]. " " .$idami["nom"]. "</strong><br/>" .$idami["publication"]. "<br/>";
+			
+				$id_doc=$idami["id_doc"];
+				
+				
+			
+				//on compte le nombre de jaimes
+				$resultat2=mysqli_query($connect, "SELECT COUNT(aime) AS nb_1 FROM reactions WHERE id_doc='$id_doc' AND aime='oui'");
+				if($resultat2)
+				{
+					while($nbaime=mysqli_fetch_array($resultat2))
+					{
+						$nb_1=$nbaime["nb_1"];
+						echo $nb_1. " j aime(s) <br/>" ;
+					}
+				}
+			
+				//on compte le nombre de partages
+				$resultat3=mysqli_query($connect, "SELECT COUNT(partage) AS nb_2 FROM reactions WHERE id_doc='$id_doc' AND partage='oui'");
+				if($resultat3)
+				{
+					while($nbpartage=mysqli_fetch_array($resultat3))
+					{
+						$nb_2=$nbpartage["nb_2"];
+						echo $nb_2. " partage(s) <br/>" ;
+					}
+				}
+			
+				//on compte le nombre de commentaires
+				$resultat4=mysqli_query($connect, "SELECT COUNT(commentaire) AS nb_3 FROM reactions WHERE id_doc='$id_doc' AND commentaire IS NOT NULL");
+				if($resultat4)
+				{
+					while($nbcommentaire=mysqli_fetch_array($resultat4))
+					{
+						$nb_3=$nbcommentaire["nb_3"];
+						echo $nb_3. " commentaire(s) <br/>" ;
+					}
+				}
+			
+				//si le document est une photo .jpg
+				if( strstr($idami["chemin"], ".jpg"))
+				{
+					echo '<img src="'.$idami["chemin"].'" alt="photo" />';
+				}
+			
+				//si le document est une video .mp4
+				if( strstr($idami["chemin"], ".mp4"))
+				{
+					echo '<video controls src="'.$idami["chemin"].'" width="800" height="500">Ici la description alternative</video>';
+				}
+				echo "<br/><br/>";
+				
+				
+				echo '<form action="newsfeed.php" method="post">';
+				echo '<input type="submit" name="'.$tour.'aimer" value="Aimer"/>';
+				echo " ";
+				echo '<input type="submit" name="'.$tour.'neplusaimer" value="Ne plus aimer"/>';
+				echo '</form>';
+				
+			
+				// si on appuie sur le bouton pour aimer
+				if (isset($_POST[$tour."aimer"]))
+				{
+					$requete1=mysqli_query($connect, "SELECT COUNT(*) AS nombre 
+													FROM reactions 
+													WHERE id_doc='$id_doc' AND id_utilisateur='$id_utilisateur'");								
+					if($requete1)
+					{
+						while($nbr=mysqli_fetch_array($requete1))
+						{
+							$nombre=$nbr["nombre"];
+							if($nombre==0) // si la ligne n'existe pas, on la crée
+							{
+								$requete2=mysqli_query($connect, "INSERT INTO reactions VALUES ('$id_doc','$id_utilisateur','oui','non',NULL)");
+							}
+							if($nombre!=0) // si la ligne existe pas, on la modifie
+							{
+								$requete2=mysqli_query($connect, "UPDATE reactions SET aime='oui' WHERE (id_doc='$id_doc' AND id_utilisateur='$id_utilisateur')");
+							}
+						}
+					}
+				}
+				
+				
+				// si on appuie sur le bouton pour ne plus aimer
+				if (isset($_POST[$tour."neplusaimer"]))
+				{
+					$requete1=mysqli_query($connect, "SELECT COUNT(*) AS nombre2 
+													FROM reactions 
+													WHERE id_doc='$id_doc' AND id_utilisateur='$id_utilisateur'");								
+					if($requete1)
+					{
+						while($nbr=mysqli_fetch_array($requete1))
+						{
+							$nombre=$nbr["nombre2"];
+							if($nombre!=0) // si la ligne existe, on la modifie
+							{
+								$requete2=mysqli_query($connect, "UPDATE reactions SET aime='non' WHERE (id_doc='$id_doc' AND id_utilisateur='$id_utilisateur')"); //OK
+							}
+						}
+					}
+				}
+				
+				
+				
+				echo "<br/><hr><br/>";
+				
+			}//fin du if d'affichage
+			
+
+		} //fin du while requete
+		
+		
+	} //fin du if requete		
+	
+	
+	
+}	//fin du else	
 ?>
