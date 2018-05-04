c8a02dcf235dbd10f82ed10f3fefddbc7e54275d
awaoulale <36473458+awaoulale@users.noreply.github.com>
2018-05-05 01:00:19 +0200

    Add files via upload
    il manque juste le code a exécuter en cas de partage

--- accueil.php
+++ accueil.php
@@ -4,113 +4,26 @@ session_start(); // On démarre la session AVANT toute chose
 
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
-		<meta charset="utf-8">
-		<title> ECEPlouf</title>
-		<meta name="generator" content="Bootply" />
-		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-		<link href="css/bootstrap.min.css" rel="stylesheet">
-		<!--[if lt IE 9]>
-			<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
-		<![endif]-->
-		<link href="css/styles.css" rel="stylesheet">
-	</head>
+
 	<body>
 <!-- Wrap all page content here -->
 <div id="wrap">
 
  
-<header class="masthead">
-
-  	<!-- Carousel
-    ================================================== -->
-    <div id="myCarousel" class="carousel slide">
-      <!-- Indicators -->
-      <ol class="carousel-indicators">
-        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-        <li data-target="#myCarousel" data-slide-to="1"></li>
-        <li data-target="#myCarousel" data-slide-to="2"></li>
-      </ol>
-      <div class="carousel-inner">
-        <div class="item active">
-          <img src="reseauprofessionnel.jpg" alt="Reseau professionnel">
-          <div class="container">
-            <div class="carousel-caption">
-              <h2>Elargissez votre reseau professionnel !</h2>
-              <p></p>
-            </div>
-          </div>
-        </div>
-        <div class="item">
-          <img src="offresprofessionnelles.jpg" alt="Offres professionnelles">
-          <div class="container">
-            <div class="carousel-caption">
-              <h2>Accedez a de nombreuses offres professionnelles !</h2>
-              <p></p>
-            </div>
-          </div>
-        </div>
-        <div class="item">
-          <img src="partageechange.jpg" alt="Partage et echange">
-          <div class="container">
-            <div class="carousel-caption">
-              <h2>Partagez &amp; Echangez !</h2>
-              <p></p>
-            </div>
-          </div>
-        </div>       
-      </div><!-- /.carousel-inner -->
-      <div class="logo"><b><font size="20">ECEPlouf</font></b></div> 
-      <!-- Controls -->
-      <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left"></span>
-      </a>
-      <a class="right carousel-control" href="#myCarousel" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right"></span>
-      </a>  
-    </div>
-    <!-- /.carousel -->
-  
-</header>
   
   
-<!-- Fixed navbar -->
-<div class="navbar navbar-custom navbar-inverse navbar-static-top" id="nav">
-    <div class="container">
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-      </div>
-      <div class="collapse navbar-collapse">
-        <ul class="nav navbar-nav nav-justified">
-          <li><a href="http://localhost:8888/OK/accueil/accueil.php">Accueil</a></li>
-          <li><a href="http://localhost:8888/OK/accueil/monreseau.php">Mon reseau</a></li>
-          <li><a href="http://localhost:8888/OK/accueil/vous.php">Vous</a></li>
-          <li><a href="http://localhost:8888/OK/accueil/notifications.php">Notifications</a></li>
-          <li><a href="http://localhost:8888/OK/accueil/emplois.php">Emplois</a></li>
-          <li><a href="http://localhost:8888/OK/accueil/messagerie.php">Messagerie</a></li>
-        </ul>
-      </div><!--/.nav-collapse -->
-    </div><!--/.container -->
-</div><!--/.navbar -->
+<?php include("fixednavbar.php"); ?>
   
 <!-- Begin page content -->
 
 <div class="container">
   <div class="col-sm-10 col-sm-offset-1">
     <div class="page-header text-center">
-      <h1>Boostez votre carriere</h1>
+      <h2>Boostez votre carriere</h2>
     </div>
     
     <p class="lead text-center"> 
-      L'ECE Paris vous propose une platforme entierement dediee a votre parcours professionnel
+      L'ECE Paris vous propose une plateforme entierement dediee a votre parcours professionnel
     </p> 
     
     <hr>
@@ -122,91 +35,281 @@ session_start(); // On démarre la session AVANT toute chose
 <div class="row">
   	<div class="col-sm-10 col-sm-offset-1">
   	  <h1> <?php 	echo "Bienvenue" . " " . $_SESSION['pseudo'] . "!" . "<br/>";  ?> </h1>
-      <h2>Votre Newsfeed</h2>
-      
-      <hr>
-      <p>Publications</p> 
-      <p>Photos</p> 
-      <p>Videos</p> 
-      <hr>
-      
+ 
       <div class="divider"></div>
       
   	</div><!--/col-->
 </div><!--/container-->
 
-
 <div class="bg-4">
   <div class="container">
-	<div class="row">
-		
+	<div class="row">	
 	
 		<div class="col-sm-12 col-xs-6"> 
 	    <div class="panel panel-default">
 	    	<div class="panel-body">
-				 <p> <b>
-				 	<?php include("newsfeed.php"); ?>
-				 </b></p>
+	    	<h2>Votre Newsfeed</h2>
+      			<hr><hr>
+				 <p>
+				 	
+				 	
+				 	
+				 	
+				 	<?php
+
+session_start(); // On démarre la session AVANT toute chose
+
+
+	$serveur="localhost";
+	$log="root";
+	$mdp="root";
+
+	$bdd="projetweb";
+	$connect=mysqli_connect($serveur,$log,$mdp);
+	$con=mysqli_select_db($connect,$bdd);
+
+	$mail=$_SESSION['mail'];
+	$pseudo=$_SESSION['pseudo'];
+
+if(!$connect )
+	echo"pb de connexion";
+else
+{
+		
+	$resultat1=mysqli_query($connect, "SELECT id_ami1, chemin, nom, prenom, publication, aime, partage, commentaire, reactions.id_doc, utilisateurs.id
+										FROM amis 
+										JOIN documents ON amis.id_ami1=documents.id_auteur 
+										JOIN utilisateurs ON utilisateurs.id=amis.id_ami1 
+										JOIN reactions ON reactions.id_doc=documents.id_doc
+										WHERE id_ami2=(SELECT id FROM utilisateurs WHERE pseudo='$pseudo') 
+										ORDER BY publication DESC");
+
+	if($resultat1)
+	{
+		$tableau=array("");
+		$tour=0;		
+		
+		
+		$sql=mysqli_query($connect,"SELECT id FROM utilisateurs WHERE pseudo='$pseudo'");
+		if($sql)
+		{
+			$ressql=mysqli_fetch_array($sql);
+			$id_utilisateur=$ressql["id"];
+		}
+		
+		
+		while($idami=mysqli_fetch_array($resultat1))
+		{
+			$tour=$tour+1;
+			
+			$tableau2=array($idami["chemin"]);
+			
+			//condition pour ne pas afficher 2 fois la meme publication dans le fil d'actualite !
+			if (!(in_array($idami["chemin"], $tableau)))
+			{
+				$tableau=array_merge($tableau, $tableau2);
+		
+				echo "<strong>" .$idami["prenom"]. " " .$idami["nom"]. "</strong><br/>" .$idami["publication"]. "<br/>";
+			
+				$id_doc=$idami["id_doc"];
+				
+				
+			
+				//on compte le nombre de jaimes
+				$resultat2=mysqli_query($connect, "SELECT COUNT(aime) AS nb_1 FROM reactions WHERE id_doc='$id_doc' AND aime='oui'");
+				if($resultat2)
+				{
+					while($nbaime=mysqli_fetch_array($resultat2))
+					{
+						$nb_1=$nbaime["nb_1"];
+						echo $nb_1. " j aime(s) <br/>" ;
+					}
+				}
+			
+				//on compte le nombre de partages
+				$resultat3=mysqli_query($connect, "SELECT COUNT(partage) AS nb_2 FROM reactions WHERE id_doc='$id_doc' AND partage='oui'");
+				if($resultat3)
+				{
+					while($nbpartage=mysqli_fetch_array($resultat3))
+					{
+						$nb_2=$nbpartage["nb_2"];
+						echo $nb_2. " partage(s) <br/>" ;
+					}
+				}
+			
+				//on compte le nombre de commentaires
+				$resultat4=mysqli_query($connect, "SELECT COUNT(commentaire) AS nb_3 FROM reactions WHERE id_doc='$id_doc' AND commentaire IS NOT NULL");
+				if($resultat4)
+				{
+					while($nbcommentaire=mysqli_fetch_array($resultat4))
+					{
+						$nb_3=$nbcommentaire["nb_3"];
+						echo $nb_3. " commentaire(s) <br/>" ;
+					}
+				}
+				
+				//affichage des commentaires				
+				$commentaires=mysqli_query($connect, "SELECT commentaire, pseudo
+														FROM reactions 
+														JOIN utilisateurs ON utilisateurs.id=reactions.id_utilisateur
+														WHERE id_doc='$id_doc' AND commentaire IS NOT NULL");
+				if($commentaires)
+				{
+					while($affichecommentaire=mysqli_fetch_array($commentaires))
+					{
+						$commentaire=$affichecommentaire["commentaire"];												
+						$auteur=$affichecommentaire["pseudo"]; //PB
+						echo '"'.$commentaire.'" par '.$auteur.'<br/>';
+					}
+				}
+				
+				
+			
+				//si le document est une photo .jpg
+				if( strstr($idami["chemin"], ".jpg"))
+				{
+					echo '<img src="'.$idami["chemin"].'" alt="photo" />';
+				}
+			
+				//si le document est une video .mp4
+				if( strstr($idami["chemin"], ".mp4"))
+				{
+					echo '<video controls src="'.$idami["chemin"].'" width="800" height="500">Ici la description alternative</video>';
+				}
+				
+				echo "<br/><br/>";
+				
+				//mise en place des boutons
+				echo '<form action="accueil.php" method="post">';
+				//bouton aimer
+				echo '<input type="submit" name="'.$tour.'aimer" value="Aimer"/>';
+				echo " ";
+				//bouton ne plus aimer
+				echo '<input type="submit" name="'.$tour.'neplusaimer" value="Ne plus aimer"/>';
+				echo "<br/><br/>";
+				//champ de texte pour le commentaire
+				//echo'<label>Entrez un commentaire</label>'; //moche
+				echo '<input type="text" name="'.$tour.'commentaire" />';
+				echo " ";
+				//bouton commenter
+				echo '<input type="submit" name="'.$tour.'commenter" value="Commenter"/>';
+				echo "<br/><br/>";
+				//bouton commenter
+				echo '<input type="submit" name="'.$tour.'partager" value="Partager"/>';
+				echo '</form>';
+			
+				// si on appuie sur le bouton pour aimer
+				if (isset($_POST[$tour."aimer"]))
+				{
+					$requete1=mysqli_query($connect, "SELECT COUNT(*) AS nombre 
+													FROM reactions 
+													WHERE id_doc='$id_doc' AND id_utilisateur='$id_utilisateur'");								
+					if($requete1)
+					{
+						while($nbr=mysqli_fetch_array($requete1))
+						{
+							$nombre=$nbr["nombre"];
+							if($nombre==0) // si la ligne n'existe pas, on la crée
+							{
+								$requete2=mysqli_query($connect, "INSERT INTO reactions VALUES ('$id_doc','$id_utilisateur','oui','non',NULL)");
+
+							}
+							if($nombre!=0) // si la ligne existe, on la modifie
+							{
+								$requete2=mysqli_query($connect, "UPDATE reactions SET aime='oui' WHERE (id_doc='$id_doc' AND id_utilisateur='$id_utilisateur')");
+							}
+						}
+					}
+				}
+				
+				// si on appuie sur le bouton pour ne plus aimer
+				if (isset($_POST[$tour."neplusaimer"]))
+				{
+					$requete1=mysqli_query($connect, "SELECT COUNT(*) AS nombre2 
+													FROM reactions 
+													WHERE id_doc='$id_doc' AND id_utilisateur='$id_utilisateur'");								
+					if($requete1)
+					{
+						while($nbr=mysqli_fetch_array($requete1))
+						{
+							$nombre=$nbr["nombre2"];
+							if($nombre!=0) // si la ligne existe, on la modifie
+							{
+								$requete2=mysqli_query($connect, "UPDATE reactions SET aime='non' WHERE (id_doc='$id_doc' AND id_utilisateur='$id_utilisateur')"); //OK
+							}
+						}
+					}
+				}
+				
+				// si on appuie sur le bouton pour commenter (ajouter un commentaire)
+				if (isset($_POST[$tour."commenter"]))
+				{
+					//echo "boutonok";
+					$nvcom=$_POST[$tour."commentaire"];
+					echo "$nvcom";
+					$requete1=mysqli_query($connect, "SELECT COUNT(*) AS nombre 
+													FROM reactions 
+													WHERE id_doc='$id_doc' AND id_utilisateur='$id_utilisateur'");								
+					if($requete1)
+					{
+						while($nbr=mysqli_fetch_array($requete1))
+						{
+							$nombre=$nbr["nombre"];
+							if($nombre==0) // si la ligne n'existe pas, on la crée
+							{
+								$requete2=mysqli_query($connect, "INSERT INTO reactions VALUES ('$id_doc','$id_utilisateur','non','non','$nvcom')");
+							}
+							if($nombre!=0) // si la ligne existe, on la modifie (concatenation avec les commentaires precedents)
+							{
+								$requete2=mysqli_query($connect, "UPDATE reactions SET commentaire=CONCAT(commentaire, ' | ', '$nvcom') WHERE (id_doc='$id_doc' AND id_utilisateur='$id_utilisateur')");
+							}
+							
+							
+						}
+					}
+				}
+				
+				
+				
+				echo "<br/><hr><br/>";
+				
+			}//fin du if d'affichage
+			
+
+		} //fin du while requete
+		
+		
+	} //fin du if requete		
+	
+	
+	
+}	//fin du else	
+?>
+				 	
+				 	
+				 	
+				 	
+				 	
+				 	
+				 </p>
 			</div>
 
 		</div><!--/panel-->
 		</div><!--/col-->
       
-      
-      
 	</div><!--/row-->
   </div><!--/container-->
 </div>
 
+
+
 <div class="divider" id="section4"></div>
 
-<div id="footer">
-  <div class="container">
-    <p class="text-muted">Copyright ©2018 ECE Paris, A. OULALE - M. TREUVELOT - E. VERMEULEN</p>
-  </div>
-</div>
+<?php include("footer.php"); ?>
 
 <ul class="nav pull-right scroll-top">
   <li><a href="#" title="Scroll to top"><i class="glyphicon glyphicon-chevron-up"></i></a></li>
 </ul>
 
-
-<div class="modal" id="myModal" role="dialog">
-  <div class="modal-dialog">
-  <div class="modal-content">
-	<div class="modal-header">
-		<button class="close" type="button" data-dismiss="modal">×</button>
-		<h3 class="modal-title"></h3>
-	</div>
-	<div class="modal-body">
-		<div id="modalCarousel" class="carousel">
- 
-          <div class="carousel-inner">
-           
-          </div>
-          
-          <a class="carousel-control left" href="#modaCarousel" data-slide="prev"><i class="glyphicon glyphicon-chevron-left"></i></a>
-          <a class="carousel-control right" href="#modalCarousel" data-slide="next"><i class="glyphicon glyphicon-chevron-right"></i></a>
-          
-        </div>
-	</div>
-	<div class="modal-footer">
-		<button class="btn btn-default" data-dismiss="modal">Close</button>
-	</div>
-   </div>
-  </div>
-</div>
-
-
-	<!-- script references -->
-		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
-		<script src="js/bootstrap.min.js"></script>
-		<script src="https://maps.googleapis.com/maps/api/js?sensor=false&extension=.js&output=embed"></script>
-		<script src="js/scripts.js"></script>
-		
-	<!-- jQuery -->
-   		 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-    <!-- Javascript de Bootstrap -->
-   		 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 	</body>
 </html>
