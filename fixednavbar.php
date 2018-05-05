318106121205aa3a2ecba84da90dbc1f5d8f925b
eleverme <36473656+eleverme@users.noreply.github.com>
2018-05-05 15:25:24 +0200

    Add files via upload

--- fixednavbar.php
+++ fixednavbar.php
@@ -13,31 +13,68 @@
 		<link href="css/styles.css" rel="stylesheet">
 </head>
 	
-<!-- Fixed navbar -->
-<div class="navbar navbar-custom navbar-inverse navbar-static-top" id="nav">
-    <div class="container">
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-      </div>
-      <div class="collapse navbar-collapse">
-        <ul class="nav navbar-nav nav-justified">
-          <li><a href="http://localhost:8888/OK/projetWeb/pages/accueil.php">Accueil</a></li>
-          <li><a href="http://localhost:8888/OK/projetWeb/pages/monreseau.php">Mon reseau</a></li>
-          <li><a href="http://localhost:8888/OK/projetWeb/pages/vous.php">Vous</a></li>
-          <li><a href="http://localhost:8888/OK/projetWeb/pages/notifications.php">Notifications</a></li>
-          <li><a href="http://localhost:8888/OK/projetWeb/pages/emplois.php">Emplois</a></li>
-          <li><a href="http://localhost:8888/OK/projetWeb/pages/messagerie.php">Messagerie</a></li>
-        </ul>
-      </div><!--/.nav-collapse -->
-    </div><!--/.container -->
-</div><!--/.navbar -->
+
+<?php
+ //session_start();
+$serveur="localhost";
+$log="root";
+$mdp="";
+$saut="</br>";
+$bdd="projetweb";
+$connect=mysqli_connect($serveur,$log,$mdp);
+$con=mysqli_select_db($connect,$bdd);
+$mail=$_SESSION['mail'];
+$pseudo=$_SESSION['pseudo'];
+
+
+
+//<!-- Fixed navbar -->
+echo'<div class="navbar navbar-custom navbar-inverse navbar-static-top" id="nav">';
+ echo'   <div class="container">';
+ echo'     <div class="navbar-header">';
+ echo'       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">';
+ echo'         <span class="icon-bar"></span>';
+ echo'         <span class="icon-bar"></span>';
+ echo'         <span class="icon-bar"></span>';
+ echo'         <span class="icon-bar"></span>';
+ echo'         <span class="icon-bar"></span>';
+ echo'         <span class="icon-bar"></span>';
+ echo'       </button>';
+ echo'     </div>';
+ echo'     <div class="collapse navbar-collapse">';
+  echo'      <ul class="nav navbar-nav nav-justified">';
+  echo'        <li><a href="accueil.html">Accueil</a></li>';
+  echo'        <li><a href="monreseau.php">Mon reseau</a></li>';
+  echo'        <li><a href="vous.php">Vous</a></li>';
+  echo'        <li><a href="notifications.php">Notifications</a></li>';
+  echo'        <li><a href="emplois.php">Emplois</a></li>';
+   echo'        <li><a href="deconnexion.php">Deconnexion</a></li>';
+  
+ $sql="SELECT type FROM utilisateurs WHERE mail='$mail' AND pseudo='$pseudo'";
+  
+  	$res= mysqli_query($connect,$sql);
+	
+	if($res){
+		    $data=mysqli_fetch_assoc($res);
+	        if($data["type"]=="Auteur"){}
+			else{
+				echo'<li><a href="admin.php">Admin</a></li>';
+				}
+	}
+ 
+  
+  echo'      </ul>';
+  echo'    </div><!--/.nav-collapse -->';
+  echo'  </div><!--/.container -->';
+echo'</div><!--/.navbar -->';
+
+
+
+?>
+
+
+
+
 
 
 </html>
